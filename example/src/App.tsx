5d15ebb32b672389a25f2c40d0dc2b3efad0ce34
nand-ini-0 <nandini.tandon@healthflex.in>
2024-12-30 19:25:24 +0530

    feat: activity-rings

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,43 +1,86 @@
-import { View, StyleSheet } from 'react-native';
-import { Text, ThemeProvider, Chip, Card, TextField, CircularProgress } from '@teamhealthflex/ui';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import {
+  Text,
+  Card,
+  Chip,
+  TextField,
+  ActivityRing,
+  ThemeProvider,
+  CircularProgress,
+} from '@teamhealthflex/ui';
 
 import { storage } from './storage';
 
 export default function App() {
+  const rings = [
+    {
+      value: 75,
+      legendText: 'Ring 1',
+      activeStrokeColor: '#48D6E0',
+      inActiveStrokeColor: '#E0E0E0',
+      activeStrokeSecondaryColor: '#2AEEA2',
+    },
+    {
+      value: 50,
+      legendText: 'Ring 2',
+      activeStrokeColor: '#FF5733',
+      inActiveStrokeColor: '#E0E0E0',
+      activeStrokeSecondaryColor: '#FFC300',
+    },
+    {
+      value: 25,
+      legendText: 'Ring 3',
+      activeStrokeColor: '#C70039',
+      inActiveStrokeColor: '#E0E0E0',
+      activeStrokeSecondaryColor: '#900C3F',
+    },
+  ];
+
   return (
     <ThemeProvider storage={storage} defaultTheme="light" storageKey="HF_THEME_STORAGE">
       <View style={styles.container}>
-        {/* Demo Text Component */}
-        <Text>text demo component</Text>
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          {/* Demo Text Component */}
+          <Text>text demo component</Text>
 
-        {/* Demo Chip Component */}
-        <Chip text="Demo Chip" size="md" preset="info" variant="outline" style={styles.chip} />
+          {/* Demo Chip Component */}
+          <Chip text="Demo Chip" size="md" preset="info" variant="outline" style={styles.chip} />
 
-        {/* Demo Card Component */}
-        <Card
-          heading="Card Title"
-          content="This is the content of the card."
-          footer="Footer text"
-          raised={true}
-          style={styles.card}
-        />
+          {/* Demo Card Component */}
+          <Card
+            heading="Card Title"
+            content="This is the content of the card."
+            footer="Footer text"
+            raised={true}
+            style={styles.card}
+          />
 
-        {/* Demo TextField Component */}
-        <View style={styles.inputContainer}>
-          <TextField label="Name" placeholder="Enter your name" style={styles.input} />
-        </View>
+          {/* Demo TextField Component */}
+          <View style={styles.inputContainer}>
+            <TextField label="Name" placeholder="Enter your name" style={styles.input} />
+          </View>
 
-        {/* Demo CircularProgress Component */}
-        <CircularProgress
-          size={100}
-          progress={75}
-          centerText="75%"
-          strokeWidth={10}
-          primaryColor="#48D6E0"
-          secondaryColor="#2AEEA2"
-          backgroundStrokeColor="#E0E0E0"
-          centerTextStyle={styles.centerText}
-        />
+          {/* Demo CircularProgress Component */}
+          <CircularProgress
+            size={100}
+            progress={75}
+            centerText="75%"
+            strokeWidth={10}
+            primaryColor="#48D6E0"
+            secondaryColor="#2AEEA2"
+            backgroundStrokeColor="#E0E0E0"
+            centerTextStyle={styles.centerText}
+          />
+
+          {/* Demo ActivityRing Component */}
+          <ActivityRing
+            rings={rings}
+            radius={100}
+            showValueText={true}
+            title="Activity Progress"
+            containerStyle={styles.activityRingContainer}
+          />
+        </ScrollView>
       </View>
     </ThemeProvider>
   );
@@ -50,17 +93,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  scrollViewContent: {
+    marginVertical: 20,
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   chip: {
     marginVertical: 20,
   },
   card: {
-    marginVertical: 20,
     width: '90%',
+    marginVertical: 20,
   },
   inputContainer: {
     width: '100%',
-    paddingHorizontal: 16,
     marginVertical: 20,
+    paddingHorizontal: 16,
   },
   input: {
     height: 50,
@@ -72,7 +121,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
   },
   centerText: {
-    color: 'black',
     fontSize: 12,
+    color: 'black',
+  },
+  activityRingContainer: {
+    marginVertical: 20,
+    alignItems: 'center',
   },
 });
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export * from './text';
 export * from './chip';
 export * from './card';
 export * from './text-field';
+export * from './activity-rings';
 export * from './circular-progress';
